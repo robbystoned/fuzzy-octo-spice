778ddf5a43990099c82363182626b4875e4942cf
robbystoned <robbystoned@gmail.com>
2014-04-14 21:38:39 +0200

    initial commit
    initial commit

---
+++ NameReverser/NameReverser/AppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.m
+//  NameReverser
+//
+//  Created by Robert vd Steen on 14-04-14.
+//  Copyright (c) 2014 Robert vd Steen. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
---
+++ NameReverser/NameReverser/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  NameReverser
+//
+//  Created by Robert vd Steen on 14-04-14.
+//  Copyright (c) 2014 Robert vd Steen. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[])
+{
+    return NSApplicationMain(argc, argv);
+}
